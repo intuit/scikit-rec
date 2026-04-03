475befb7845f05b4f181f38ea99c5d795c3b4fb2
Shankar Sankararaman <shankar.sankararaman@gmail.com>
2026-04-02 23:29:02 -0700

    fix: exclude unreachable PackageNotFoundError branch from coverage

--- skrec/__init__.py
+++ skrec/__init__.py
@@ -4,5 +4,5 @@ from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("scikit-rec")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
